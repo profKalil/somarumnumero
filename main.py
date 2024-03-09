4bc4edd2533b74de43f53032fbf91469cf5deaeb
profKalil <84177112+profKalil@users.noreply.github.com>
2024-03-09 14:13:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Variaveis e valores
+num1 = 7
+num2 = 1
+
+# Processos
+soma = num1 + num2
+
+# Saída
+print(f"A soma de {num1} e {num2} é {soma}")
+#print("A soma de {} e {} é {}".format(num1, num2, soma))
+#print("A soma de", num1, "e", num2, "é", soma)
+#print("A soma de " + str(num1) + " e " + str(num2) + "
